36a0c946fe877df94dea252ad85d1f2a976295c9
DanieleMarino70 <danielaotitan@gmail.com>
2023-02-22 13:48:07 +0100

    add: footerLinks & vue to footer

--- js/data.js
+++ js/data.js
@@ -102,3 +102,112 @@ const headerLinks = [
         ]
     },
 ];
+
+
+const footerLinks = [
+    //Address Info i 0
+    [
+        {
+            title: "Address info"
+        },
+       {
+        icon:  "fa-location-dot", //fontawesome
+        text: "60 29th Street San Francisco, CA 94110 507-Union Trade Center, United States of America",
+        url: "https://www.google.com/search?q=60+29th+Street+San+Francisco%2C+CA+94110+507-Union+Trade+Center%2C+United+States+of+America&rlz=1C1VDKB_itIT1022IT1022&oq=60+29th+Street+San+Francisco%2C+CA+94110+507-Union+Trade+Center%2C+United+States+of+America&aqs=chrome..69i57.352j0j7&sourceid=chrome&ie=UTF-8"
+       },
+       {
+        icon: "fa-phone",
+        text: "(+00) 123-456-789",
+        url: "tel:+00123456789"
+       },
+       {
+        icon: "fa-envelope",
+        text: "demo@example.com",
+        url: "mailto:demo@example.com"
+       }
+    ],
+    
+    //Information i 1
+    [
+        {
+            title: "Information"
+        },
+        {
+            text: "Bookmark",
+            url: "#",  //path
+        },
+        {
+            text: "Features",
+            url: "#",  //path
+        },
+        {
+            text: "Sitemap",
+            url: "#",  //path
+        },
+        {
+            text: "Lists",
+            url: "#",  //path
+        },
+        {
+            text: "Contact Us",
+            url: "#",  //path
+        },
+
+    ],
+
+    //Extras i 2
+    [
+        {
+            title: "Extras"
+        },
+        {
+            text: "Delivery",
+            url: "#",  //path
+        },
+        {
+            text: "Cart",
+            url: "#",  //path
+        },
+        {
+            text: "Terms conditions",
+            url: "#",  //path
+        },
+        {
+            text: "My account",
+            url: "#",  //path
+        },
+        {
+            text: "About Us",
+            url: "#",  //path
+        },
+    ],
+
+    // Newsletter i 3
+    [
+        {
+            title: "Our Newsletter",
+            text: "There are many variations of passages of form humour or randomised"
+        },
+        {
+            icon: "fa-facebook-f",
+            url: "#" //path
+        },
+        {
+            icon: "fa-twitter",
+            url: "#" //path
+        },
+        {
+            icon: "fa-google-plus-g",
+            url: "#" //path
+        },
+        {
+            icon: "fa-skype",
+            url: "#" //path
+        },
+        {
+            icon: "fa-instagram",
+            url: "#" //path
+        },
+    ]
+
+];
--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ const app = createApp({
     data(){
         return {
             headerLinks: headerLinks,
+            addressInfo: footerLinks[0],
+            information: footerLinks[1],
+            extras: footerLinks[2],
+            newsletter: footerLinks[3],
         };
     },
 
