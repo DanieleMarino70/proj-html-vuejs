5ca4d278cee061c8f741772a205534fb1f67ae95
DanieleMarino70 <danielaotitan@gmail.com>
2023-02-22 11:45:05 +0100

    add: vue & headerLinks

---
+++ js/data.js
@@ -0,0 +1,104 @@
+const headerLinks = [
+    {
+        title: "Home",
+        menuLists: [
+            {
+                subtitle: "Left Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Right Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Full Width",
+                url: "#"
+            },
+        ]
+    },
+    {
+        title: "Shop",
+        menuLists: [
+            {
+                subtitle: "Left Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Right Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Full Width",
+                url: "#"
+            },
+        ]
+    },
+    {
+        title: "Blog",
+        menuLists: [
+            {
+                subtitle: "Left Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Right Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Full Width",
+                url: "#"
+            },
+        ]
+    },
+    {
+        title: "Media",
+        menuLists: [
+            {
+                subtitle: "Left Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Right Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Full Width",
+                url: "#"
+            },
+        ]
+    },
+    {
+        title: "ShortCode",
+        menuLists: [
+            {
+                subtitle: "Left Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Right Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Full Width",
+                url: "#"
+            },
+        ]
+    },
+    {
+        title: "Features",
+        menuLists: [
+            {
+                subtitle: "Left Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Right Sidebar",
+                url: "#"
+            },
+            {
+                subtitle: "Full Width",
+                url: "#"
+            },
+        ]
+    },
+];
